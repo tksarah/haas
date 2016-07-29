5899212ecefae1dba83e654a79d59eddbffe9c43
tksarah <tsarah8139@gmail.com>
2016-07-29 14:33:57 +0900

    Update lib.pl

--- lib.pl
+++ lib.pl
@@ -220,13 +220,14 @@ sub userlist{
 	my %data = @_;
 	my $k;
 	my $v;
+	my $max_emp = get_value('max_emp');
 	my @list;
 
 print "<h3>現在の利用状況</h3><br>";
 
 if(keys %data == 0){
 	print "利用者がいません。<p>\n";
-}elsif(keys %data == 10){
+}elsif(keys %data == $max_emp){
 	print "現在<font color=\"red\">フル稼働</font>です。1時間以上お待ち下さい。\n";
 	print "<table>\n";
 	print "<tr><th>User name</th><th>Lesson</th><th>Start time</th><th>End time</th></tr>\n";
