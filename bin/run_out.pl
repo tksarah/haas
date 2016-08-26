5125f48a867cb7ef0861e33684063ab85e3f06db
tksarah <tsarah8139@gmail.com>
2016-08-26 16:07:29 +0900

    Add bin

---
+++ bin/run_out.pl
@@ -0,0 +1,95 @@
+#!/usr/bin/perl
+
+require '../lib.pl';
+use strict;
+use CGI;
+use DateTime;
+
+
+# Get Date
+my $dt = DateTime->now(time_zone => 'Asia/Tokyo');
+my $now_month = $dt->month;
+my $year;
+my $month;
+my $name;
+my @dep = ("A","B","C");
+my %dep_hash;
+# Set logfiles
+my $logfile = "../data/haas.log";
+my $datadir = "../data";
+my $archivedir = "$datadir/archives/";
+
+# From arg
+if($ARGV[0] =~ /\d{2}/){
+	$month = $ARGV[0];
+}else{
+	$month = "0$now_month";
+}
+if($ARGV[1] =~ /\d{4}/){
+	$year = $ARGV[1];
+}else{
+	$year = $dt->year;
+}
+
+foreach $name (@dep){
+	my $listfile = "../data/$name.list";
+	my $ym = "$year-$month";
+	if( -f "$archivedir/$year-$month-$name.log"){
+		print "$year-$month-$name.log exists.\n";
+	}else{
+		print "$year-$month-$name ... ";
+		# Create Hash from a department list
+		%dep_hash = &create_hash($listfile);
+
+		foreach my $user ( sort keys %dep_hash ){
+		        open(R,"<$logfile");
+       		 	while (<R>) {
+
+				# date hands-on has been started
+                                my $date = (split/,/,$_)[2];
+                                my $id = (split/,/,$_)[0];
+
+				if( $date =~ /$ym/ && $id =~ /$user/ ){
+					open(W,">>$datadir/archives/$year-$month-$name.log");
+					print W "$_";
+					close(W);
+				}
+			}
+			close(R);
+		}
+		if( !-f "$archivedir/$year-$month-$name.log"){
+			print "No count.\n";
+		}else{
+			print "Created.\n";
+		}
+	}
+
+}
+
+exit(0);
+
+sub create_hash{
+	my $file = shift;
+	my $dep_id;
+	my $username;
+	my %hash;
+
+	open(R,"<$file");
+	while (<R>) {
+		if( /,/ ){
+       			$_ =~ s/\s+//g;
+               		$dep_id = (split/,/,$_)[0];
+       			$username = (split/,/,$_)[1];
+			chomp($username);
+			if($username eq ""){
+				$hash{$dep_id} = "$dep_id";
+			}else{
+				$hash{$dep_id} = "$username";
+			}
+		}else{
+			chomp($_);
+			$hash{$_} = "$_";
+		}
+	}
+	return %hash;
+}
